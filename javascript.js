4bae9491c43886d59ffe720ee3b7db7b477c123d
McKayV <mckaygit@summit8.net>
2016-05-27 08:14:37 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,11 +1,24 @@
-function getNum() {
+function buttonPress() {
 
-  document.getElementById("demo").innerHTML =
-    "Hello";
+  var input = window.prompt("Type in single digit numbers seperated by commas.");
+
+  document.getElementById("out").innerHTML =
+    "Due to technical difficulties (math is hard), all I can tell you is that for the list 1, 2, 3,
+    the mean is 2, the median is 2, and the mode is 1.";
+
+  //document.getElementById("out").innerHTML =
+  //  "The mean is " + getNum(input);
+
+}
+
+
+
+
+function getMean(var x) {
 
   var nums = [];
   for(var i = 0; i < arguments.length; i++) {
-    if(parseInt(arguments[i]) > 0) nums.push(arguments[i]);
+    nums.push(parseInt arguments[i]);
   }
 
   // Finds the mean
@@ -15,24 +28,16 @@ function getNum() {
   }
   mean /= nums.length;
 
-  // Finds the medium
-  for(var i = 0; i < nums.length; i++) {
-  }
-
-  // Finds the mode
-
-  return mean;
+  return x;
 }
 
 
-function buttonPress() {
-  var input = window.prompt("Type in single digit numbers above 0");
-
-  document.getElementById("out").innerHTML =
-    "The mean is " + getNum(input);
-
-
-
-
-
+function getMedian(var y) {
+  return 1;
 }
+
+
+function getMode(var input) {
+  return 1;
+}
+
